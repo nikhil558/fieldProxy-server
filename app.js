c1762fafdf8092d40cdc1f64fde66a8e81b5aa0f
Nikhil <nikhilnikki558@gmail.com>
2021-07-31 09:51:35 +0000

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const dbPath = path.join(__dirname, "UserData.db");
 
